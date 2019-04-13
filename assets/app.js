5165683c9f93c437324700c8cfba927d92c159ed
Jordynn Otto <j_taylor92@yahoo.com>
2019-04-13 10:57:32 -0700

    ajax

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,54 @@
-function restaurantDisplay() {
+var lat;
+var long;
 
-    // grabbing city user types in
-    var city = $("#location").val().trim();
-    console.log(city);
+// when clicking submit picture, run this API call
+$("#submitBTN").on("click", function (event) {
 
-    var APIKey = eb5059d5e18e77588ecf8134ad1603c4;
+  event.preventDefault();
 
-    var queryURL = "https://www.zomato.com/?=city_name" + city + "&apikey=" + APIKey;
+  // grabbing city user types in
+  var city = $("#location").val().trim();
+  console.log(city);
 
-    // Creates AJAX call for the specific city
+  var queryURL = "https://developers.zomato.com/api/v2.1/locations?query=" + city + "&apikey=eb5059d5e18e77588ecf8134ad1603c4";
+
+  // Creates AJAX call for the specific city
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+
+    // all data for city
+    console.log(response);
+
+    // grabbing latitude for city
+    lat = response.location_suggestions[0].latitude;
+    console.log(lat);
+
+    // grabbing longitude for city
+    long = response.location_suggestions[0].longitude;
+    console.log(long);
+
+    var queryURL2 = "https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + long + "&apikey=eb5059d5e18e77588ecf8134ad1603c4";
+
     $.ajax({
-      url: queryURL,
+      url: queryURL2,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
 
-      var results = response.data;
-      console.log(results);
+      console.log(response);
 
-    });
+      //console.log();
+      
 
-  }
+      //$(".card-text-food").append(response.restaurants)
+
+
+    })
+  });
+});
+
+
+
+
+
